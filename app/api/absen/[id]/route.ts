39c8fd6fbf26dd7c5795c958faacea64078f709f
Akmal Yudistira <akmalyudistira1212@gmail.com>
2024-08-28 02:36:52 +0900

    ubah params id

--- app/api/absen/[id]/route.ts
+++ app/api/absen/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(params: { id: string }) {
   const { id } = params;
 
   try {
