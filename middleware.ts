9205dc52fa4ee9266ba50479eeea84165e5a7389
Akmal Yudistira <akmalyudistira1212@gmail.com>
2024-09-01 23:53:20 +0900

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const allowedOrigins = ["http://localhost:3000", "https://my-app.org"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://my-app-sigma-ivory.vercel.app/",
+];
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
